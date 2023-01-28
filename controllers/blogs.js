c831ce971dd4a271110784ca88de57b9cd431c66
HeXudong <1514133606@qq.com>
2023-01-28 09:44:25 +0800

    feat:add comments

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -14,9 +15,11 @@ blogsRouter.delete('/:id', async (request, response) => {
 
   const blog = await Blog.findById(request.params.id)
   if (blog.user.toString() === request.user.id) {
-    const removedBlog =  await Blog.findByIdAndRemove(request.params.id)
+    const removedBlog = await Blog.findByIdAndRemove(request.params.id)
     const user = await User.findById(removedBlog.user)
-    user.blogs = user.blogs.filter(blog => blog.toString() !== request.params.id)
+    user.blogs = user.blogs.filter(
+      (blog) => blog.toString() !== request.params.id
+    )
     await user.save()
     response.status(204).end()
   } else {
@@ -48,8 +51,27 @@ blogsRouter.put('/:id', async (request, response) => {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
   const { title, url, likes } = request.body
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, { title, url, likes }, { new: true, runValidators: true, context: 'query' }).populate('user')
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, url, likes },
+    { new: true, runValidators: true, context: 'query' }
+  ).populate('user')
   response.json(updatedBlog)
+})
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  if (!request.user || !request.user.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const { content } = request.body
+  const blog = await Blog.findById(request.params.id)
+  const comment = {
+    content,
+    id: new mongoose.Types.ObjectId(),
+  }
+  blog.comments.push(comment)
+  await blog.save()
+  response.json(comment)
 })
 
 module.exports = blogsRouter
